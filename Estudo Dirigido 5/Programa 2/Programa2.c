c1fd60d2158093acb2e9e418696154ba3989e456
Camargo <110298379+MatheusCamargoGinebro@users.noreply.github.com>
2022-11-19 12:50:15 -0300

    Add files via upload

---
+++ Estudo Dirigido 5/Programa 2/Programa2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+/*
+O=======================================================================O
+| Estudo dirigido 5 - Programa 2 - Sistema de login com Lista encadeada |
+|    Nomes:                                                             |
+|        Matheus Camargo Ginebro CP3016153                              |
+|        Lucas Haiter Leoni CP3017362                                   |
+O=======================================================================O
+*/
+
+
+/*------------------------------< Structs >------------------------------*/
+
+
+
+
+
+
+
+
+/*------------------------------< Functions >------------------------------*/
+
+
+
+
+
+
+
+/*------------------------------< Main >------------------------------*/
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+
+    
+
+
+    system("pause");
+    return 0;
+}
