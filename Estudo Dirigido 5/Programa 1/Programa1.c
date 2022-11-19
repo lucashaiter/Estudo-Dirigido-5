51f1b2f5c7f5f146242f2f4c82269527ebf7c645
MatheusCamargoGinebro <matheuscginrbro@gmail.com>
2022-11-19 15:28:54 -0300

    'teste--

---
+++ Estudo Dirigido 5/Programa 1/Programa1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+/*
+O=======================================================O
+| Estudo dirigido 5                                     |
+| Programa 2 - Sistema de cadastro acadêmico aprimorado |
+|    Nomes:                                             |
+|        Matheus Camargo Ginebro CP3016153              |
+|        Lucas Haiter Leoni CP3017362                   |
+O=======================================================O
+*/
+
+int main(){
+    setlocale(LC_ALL, "portuguese");
+
+    
+
+
+    system("pause");
+    return 0;
+}
